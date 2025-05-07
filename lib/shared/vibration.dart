249ed2bc28cd11f52fa5302838247afcbc69b16e
Mirza <sayheymirza@gamil.com>
2025-05-07 15:56:36 +0330

    🔨  vibrate is not working on web

--- lib/shared/vibration.dart
+++ lib/shared/vibration.dart
@@ -1,10 +1,17 @@
 import 'package:chat/shared/constants.dart';
 import 'package:chat/shared/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:vibration/vibration.dart';
 
 Future<void> vibrate({int duration = 100}) {
-  if (Services.configs.get<bool>(key: CONSTANTS.STORAGE_SETTINGS_VIBRATION) ==
-      false) return Future.value();
+  if (kIsWeb) return Future.value();
+
+  if (Services.configs
+          .get(key: CONSTANTS.STORAGE_SETTINGS_VIBRATION)
+          .toString() ==
+      'false') {
+    return Future.value();
+  }
 
   return Vibration.vibrate(duration: duration);
 }
